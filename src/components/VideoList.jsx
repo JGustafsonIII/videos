a7b4d0b1153e9e31d58ca9f77d3a16680c485601
John Gustafson <John.Gustafson1@Marist.edu>
2021-02-17 19:42:42 -0500

    VideoDetail and VideoItem working

---
+++ src/components/VideoList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VideoItem from './VideoItem';
+
+const VideoList = ({ videos, onVideoSelect }) => {
+  const videoList = videos.map((video) => {
+    return <VideoItem video={video} onVideoSelect={onVideoSelect} />;
+  });
+
+  return <div className=' ui relaxed divided list'>{videoList}</div>;
+};
+
+export default VideoList;
